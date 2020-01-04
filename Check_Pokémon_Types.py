d0046103a39b065e4903596e3d9b9d47cb7b6e3c
Peste <49153232+pesterazor@users.noreply.github.com>
2020-01-04 14:37:28 +0100

    Add files via upload

--- Check_Pokémon_Types.py
+++ Check_Pokémon_Types.py
@@ -44,4 +44,49 @@ def typesMultFactor(attackType, defendType):
     return types_matrix[types.index(attackType)][types.index(defendType)]
 
 
-print(typesMultFactor("electric", "ground"))
+req = "flying,Steel"
+#req = input("Insert the opponent type/s (e.g. flying, steel):\n>>> ")
+req = req.replace(", ", ",").lower()
+req = req.split(",")
+
+noDamage = []
+vLowDamage = []
+lowDamage = []
+normDamage = []
+highDamage = []
+quadDamage = []
+i = 0
+if len(req) == 1:
+    for t in types_matrix:
+        damage = t[types.index(str(req[0]))]
+        if damage == 2.0:
+            highDamage = highDamage + [types[i]]
+        elif damage == 1.0:
+            normDamage = normDamage + [types[i]]
+        elif damage == 0.5:
+            lowDamage = lowDamage + [types[i]]
+        else:
+            noDamage = noDamage + [types[i]]
+        i = i + 1
+else:
+    for t in types_matrix:
+        damage = t[types.index(str(req[0]))] * t[types.index(str(req[0]))]
+        if damage == 4.0:
+            quadDamage = quadDamage + [types[i]]
+        elif damage == 2.0:
+            highDamage = highDamage + [types[i]]
+        elif damage == 1.0:
+            normDamage = normDamage + [types[i]]
+        elif damage == 0.5:
+            lowDamage = lowDamage + [types[i]]
+        elif damage == 0.25:
+            vLowDamage = vLowDamage + [types[i]]
+        else:
+            noDamage = noDamage + [types[i]]
+        i = i + 1
+
+print(lowDamage)
+print(normDamage)
+print(highDamage)
+print(quadDamage)
+input()
