254bc3e34f316257070b4cd54505e8d90676a241
Peste <49153232+pesterazor@users.noreply.github.com>
2020-01-04 22:33:20 +0100

    Add files via upload

--- Check_Pokémon_Types.py
+++ Check_Pokémon_Types.py
@@ -1,3 +1,5 @@
+import pytest
+
 types_matrix = [[1.0, 1.0, 1.0, 1.0, 1.0, 0.5, 1.0, 0.0, 0.5, 1.0, 1.0, 1.0,
                  1.0, 1.0, 1.0, 1.0, 1.0, 1.0],
                 [2.0, 1.0, 0.5, 0.5, 1.0, 2.0, 0.5, 0.0, 2.0, 1.0, 1.0, 1.0,
@@ -44,8 +46,7 @@ def typesMultFactor(attackType, defendType):
     return types_matrix[types.index(attackType)][types.index(defendType)]
 
 
-req = "flying,Steel"
-#req = input("Insert the opponent type/s (e.g. flying, steel):\n>>> ")
+req = input("Insert the opponent type/s (e.g. flying, steel):\n>>> ")
 req = req.replace(", ", ",").lower()
 req = req.split(",")
 assert (len(req) == 1) or (len(req) == 2)
@@ -56,6 +57,7 @@ lowDamage = []
 normDamage = []
 highDamage = []
 quadDamage = []
+
 i = 0
 if len(req) == 1:
     for t in types_matrix:
@@ -86,8 +88,35 @@ else:
             noDamage = noDamage + [types[i]]
         i = i + 1
 
-print(lowDamage)
-print(normDamage)
-print(highDamage)
-print(quadDamage)
+if quadDamage != []:
+    print("#" * 10 + " 4x damage " + "#" * 10)
+    for t in quadDamage:
+        print("- " + str(t))
+    print("")
+if highDamage != []:
+    print("#" * 10 + " 2x damage " + "#" * 10)
+    for t in highDamage:
+        print("- " + str(t))
+    print("")
+if normDamage != []:
+    print("#" * 10 + " 1x damage " + "#" * 10)
+    for t in normDamage:
+        print("- " + str(t))
+    print("")
+if lowDamage != []:
+    print("#" * 10 + " 0.5x damage " + "#" * 10)
+    for t in lowDamage:
+        print("- " + str(t))
+    print("")
+if vLowDamage != []:
+    print("#" * 10 + " 0.25x damage " + "#" * 10)
+    for t in vLowDamage:
+        print("- " + str(t))
+    print("")
+if noDamage != []:
+    print("#" * 10 + " No damage " + "#" * 10)
+    for t in noDamage:
+        print("- " + str(t))
+    print("")
+
 input()
